5ae3ae9910884efb65efc98a4cbe04db21e04fce
panarik <panariks@gmail.com>
2022-06-06 00:03:37 +0300

    add new iOS project

---
+++ Swift-IOS-Features/Swift_IOS_FeaturesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_IOS_FeaturesApp.swift
+//  Swift-IOS-Features
+//
+//  Created by panarik on 6/5/22.
+//
+
+import SwiftUI
+
+@main
+struct Swift_IOS_FeaturesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
