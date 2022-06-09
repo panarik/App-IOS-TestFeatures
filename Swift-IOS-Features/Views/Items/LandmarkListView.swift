fefa0d55d0ae431793a9447a0055ba1b8730b178
panarik <panariks@gmail.com>
2022-06-09 13:54:07 +0300

    added NavigationLink from main menu

--- Swift-IOS-Features/Views/Items/LandmarkListView.swift
+++ Swift-IOS-Features/Views/Items/LandmarkListView.swift
@@ -1,15 +1,58 @@
 import SwiftUI
 
 struct LandmarkListView: View {
+    
+    @State var isLinkActive = false
+    
     var body: some View {
-        List(landmarks) { Landmark in
-            LandmarkRow(landmark: Landmark)
+        //List with items
+        NavigationView {
+            List(landmarks) { Landmark in
+                NavigationLink {
+                    Text("Test")
+                }
+            label: {
+                    LandmarkRow(landmark: Landmark)
+                }
+            }
+            .navigationTitle("Main menu")
         }
     }
 }
+            
+    
+        
+        // Navigation Button
+//        NavigationView {
+//                    VStack(alignment: .leading) {
+//
+//                        NavigationLink(destination: Text("OtherView"), isActive: $isLinkActive) {
+//                            Button(action: {
+//                                self.isLinkActive = true
+//                            }) {
+//                                Text("Login")
+//                            }
+//                        }
+//                    }
+//                    .navigationBarTitle(Text("Login"))
+//                }
+        
+        //Navigation List
+//        NavigationView {
+//            List(landmarks) { landmark in
+//                NavigationLink {
+//                    LandmarkDetail(landmark: landmark)
+//                } label: {
+//                    LandmarkRow(landmark: landmark)
+//                }
+//            }
+//            .navigationTitle("Landmarks")
+//        }
+        
 
 struct LandmarkListView_Previews: PreviewProvider {
     static var previews: some View {
         LandmarkListView()
+        
     }
 }
