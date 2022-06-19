bddf8204a91814fbcae1caad7f41c331911bc171
panarik <panariks@gmail.com>
2022-06-20 01:35:34 +0300

    added UI tests examples

--- Swift-IOS-FeaturesUITests/Swift_IOS_FeaturesUITests.swift
+++ Swift-IOS-FeaturesUITests/Swift_IOS_FeaturesUITests.swift
@@ -8,26 +8,41 @@
 import XCTest
 
 class Swift_IOS_FeaturesUITests: XCTestCase {
+    
+    let app = XCUIApplication()
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+        app.launch()
     }
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
-        app.launch()
-
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testCheckElements() throws {
+        let logo = app.images["login-secure"]
+        
+        // Check
+        XCTAssertTrue(logo.exists)
+    }
+    
+    func testCkickButtons() throws {
+        let nameTextField = app.textFields["Your Name"]
+        let emailTextField = app.textFields["Your Email"]
+        let passTextField = app.textFields["Your Password"]
+        
+        // Step 1: Enter username
+        nameTextField.tap()
+        nameTextField.typeText("user")
+        
+        // Step 2: Enter email
+        emailTextField.tap()
+        emailTextField.typeText("test@mail.com")
+        
+        // Step 3: Enter pass
+        passTextField.tap()
+        passTextField.typeText("password")
     }
 
     func testLaunchPerformance() throws {
