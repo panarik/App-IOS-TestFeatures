b42c6e64069595cf700ee8026e9edba4b6a3620f
panarik <panariks@gmail.com>
2022-07-16 01:18:57 +0300

    init

---
+++ App-IOS-TestFeatures/App_IOS_TestFeaturesApp.swift
@@ -0,0 +1,17 @@
+//
+//  App_IOS_TestFeaturesApp.swift
+//  App-IOS-TestFeatures
+//
+//  Created by panarik on 7/16/22.
+//
+
+import SwiftUI
+
+@main
+struct App_IOS_TestFeaturesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
