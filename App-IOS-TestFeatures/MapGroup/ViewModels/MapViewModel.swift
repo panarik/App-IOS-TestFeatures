56f12a6320ef1e5b07497280daae6c1d7dc92cc3
panarik <panariks@gmail.com>
2022-07-29 01:44:51 +0300

    added map with header

--- App-IOS-TestFeatures/MapGroup/ViewModels/MapViewModel.swift
+++ App-IOS-TestFeatures/MapGroup/ViewModels/MapViewModel.swift
@@ -1,11 +1,44 @@
 import Foundation
+import SwiftUI
+import MapKit
 
 class MapViewModel: ObservableObject {
     
+    // All locations.
     @Published var locations: [Location]
     
+    // Current location.
+    @Published var mapLocation: Location {
+        
+        // do this after updating var 'mapLocation'.
+        didSet {
+            updateRegion(location: mapLocation)
+        }
+    }
+    
+    // Current region map on the screen.
+    @Published var mapRegion: MKCoordinateRegion
+    
+    // Map scale.
+    let mapScale = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+    
     init() {
-        self.locations = LocationsDataService.locations
+        let locations = LocationsDataService.locations
+        self.locations = locations
+        self.mapLocation = locations.first!
+        
+        //Make first region for map.
+        mapRegion = MKCoordinateRegion(center: locations.first!.coordinates, span: mapScale)
+    }
+    
+    /**
+     Updating map region to current location.
+     `location` - current location
+     */
+    func updateRegion(location: Location) {
+        withAnimation {
+            mapRegion = MKCoordinateRegion(center: location.coordinates, span: mapScale)
+        }
     }
     
 }
--- App-IOS-TestFeatures/MapGroup/Views/MapView.swift
+++ App-IOS-TestFeatures/MapGroup/Views/MapView.swift
@@ -1,13 +1,20 @@
 import SwiftUI
+import MapKit
 
 struct MapView: View {
     
     @StateObject var vm: MapViewModel = MapViewModel()
-    
+
     var body: some View {
-        VStack {
-            List {
-                ForEach(vm.locations) {Text($0.name)}
+        ZStack {
+            Map(coordinateRegion: $vm.mapRegion)
+                .ignoresSafeArea()
+            
+            VStack {
+                headers
+                    .padding()
+                
+                Spacer()
             }
         }
         // Creates the environment object.
@@ -21,4 +28,22 @@ struct MapView_Previews: PreviewProvider {
     static var previews: some View {
         MapView()
     }
+}
+
+extension MapView {
+    
+    private var headers: some View {
+        VStack {
+            Text(vm.mapLocation.name + ", " + vm.mapLocation.cityName)
+                .font(.title)
+                .fontWeight(.black)
+                .frame(maxWidth: .infinity)
+                .frame(height: 50)
+
+        }
+        .background(.thickMaterial)
+        .cornerRadius(10)
+        .shadow(color: .black, radius: 20, x: 0, y: 15)
+    }
+    
 }
